658f424dc9d9e3bd23dc07add99ee4343b419c55
dimas pradipto <dpradipto8@gmail.com>
2026-07-10 09:20:56 +0700

    feat: configure global locale and HTTPS scheme in AppServiceProvider and add dashboard sidebar layout component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Carbon::setlocale('id');
+        if (app()->envirotment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
